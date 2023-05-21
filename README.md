2550109557975fa65676e2d622ff3d099ec8e65e
kattlover99 <thrillseeker.nw@gmail.com>
2023-05-21 23:07:10 +0900

    first update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# texteditor-svelte
+# [`md.robino.dev`](https://md.robino.dev)
+
+## A web based markdown editor
+
+- Uses the File System API if supported (otherwise a download anchor tag)
+- Auto saves changes if supported
+- All files are saved locally only
+
+## MIT License
+
+## Open to contributions
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "../app.postcss";
+	import { dev } from "$app/environment";
+	import { inject } from "@vercel/analytics";
+
+	inject({ mode: dev ? "development" : "production" });
+</script>
+
+<div
+	class="flex h-[100dvh] flex-col bg-slate-900 text-slate-50 selection:bg-slate-300 selection:text-slate-950"
+>
+	<slot />
+</div>
